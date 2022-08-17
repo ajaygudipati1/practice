8d761481d6f82aa01d97c94d8e9ade7a5add6f79
ajaygudipati1 <110895769+ajaygudipati1@users.noreply.github.com>
2022-08-17 19:55:15 +0530

    Update LoopStatementDemo.java

--- LoopStatementDemo.java
+++ LoopStatementDemo.java
@@ -3,15 +3,13 @@ package exercise1;
 public class LoopStatementDemo {
 
 	public static void main(String[] args) {
-		for(int i=1; i<=10; i++)
-		{
-        System.out.print(i+" ");
+	    for(int i=1; i<=10; i++) {
+            System.out.print(i+" ");
 		}
-		System.out.println("");
+	    System.out.println("");
 		
 		
-	    for(int j=10; j>=1; j--)
-	    {
+	    for(int j=10; j>=1; j--) {
 	    System.out.print(j+" ");
 	    }
 	    System.out.println("");
@@ -22,8 +20,7 @@ public class LoopStatementDemo {
 	    
 	    
 	    int k = 1;
-	    do
-	    {
+	    do {
 	    System.out.print(k+++" ");
 	    }
 	    while(k<=5);
@@ -31,17 +28,14 @@ public class LoopStatementDemo {
 	    
 	    
 	    int l = 1;
-	    while(l<=5)
-	    {
+	    while(l<=5) {
 	    System.out.print(l+++" ");
 	    }
 	    System.out.println("");
 	    
 	    
-	    for(int m=2; m<=10; m++)
-	    {
-	    	if(m%2==0)
-	    	{
+	    for(int m=2; m<=10; m++) {
+	    	if(m%2==0) {
 	    	System.out.print(m+" ");
 	    	}
 	    }
