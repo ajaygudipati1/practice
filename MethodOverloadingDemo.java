21a12bbe1ca706ab08794f5497f7f7987e7d5cef
ajaygudipati1 <110895769+ajaygudipati1@users.noreply.github.com>
2022-08-17 14:09:35 +0530

    Update MethodOverloadingDemo.java

--- MethodOverloadingDemo.java
+++ MethodOverloadingDemo.java
@@ -2,27 +2,19 @@ package exercise1;
 
 public class MethodOverloadingDemo {
 
-	
-		void display(int a)
-		{	
+	        void display(int a){	
 			System.out.println(a);
 		}
-		void display(int a, int b)
-		{
+		void display(int a, int b){
 			System.out.println(a+" "+b);
 		}
-		void display(int a, double b)
-		{
+		void display(int a, double b){
 			System.out.println(a+" "+b);
 		}
-		public static void main(String[] args)
-		{
+		public static void main(String[] args){
 			MethodOverloadingDemo obj = new MethodOverloadingDemo();
 			obj.display(10);
 			obj.display(15,20);
 			obj.display(25,60.78);
-		}
-
-	
-
+		} 
 }
