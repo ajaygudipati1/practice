756c0eaaea03de6ec97eb38e02de3adf9ae70729
ajaygudipati1 <110895769+ajaygudipati1@users.noreply.github.com>
2022-08-17 19:46:01 +0530

    Add files via upload

---
+++ Basics.java
@@ -0,0 +1,22 @@
+package exercise1;
+
+public class Basics {
+
+	public static void main(String[] args){
+	System.out.println("Ajaykumar");
+	System.out.println("Ajaykumar");
+	System.out.println("17KQ1A0363");
+	System.out.print("Ajaykumar");
+	System.out.println("   17KQ1A0363");
+	System.out.println(3+3);
+	System.out.println("Pace Institute of Technology and Sciences");
+	System.out.println("hello 123");
+	int a = 20;
+	int b = 10;
+	int c = a/b;
+	System.out.println(c);
+	System.out.println( "The addition of 10 and 20 is : 30");
+
+	}
+
+}
