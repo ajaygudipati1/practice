a084fd6b6056a600b7c356ca27b9d49d85face0e
ajaygudipati1 <110895769+ajaygudipati1@users.noreply.github.com>
2022-08-25 16:14:16 +0530

    Update StringSample.java

--- StringSample.java
+++ StringSample.java
@@ -5,12 +5,13 @@ public class StringSample {
 	public static void main(String[] args) {
 		String name ="ajay";
 		int count = 0;
-		for(int i=0; i<=name.length(); i++) {
-			for(int j=0; j<=name.length(); j++) {
+		for(int i=0; i<name.length(); i++) {
+			for(int j=0; j<name.length(); j++) {
 				if(name.charAt(i)==name.charAt(j)) {
 					count++;
 				}
 				System.out.println(name.charAt(i)+" : "+count);
+				count = 0;
 			}
 		}
 	}
